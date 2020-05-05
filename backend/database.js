16a2e942629519da08e55f40d14647ebc5fb3613
Chris Haack <chris.haack@me.com>
2020-05-05 10:11:28 -0400

    change cluster

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://jasper:7wEEQc0osVFqK2Yj@mern-csq5x.mongodb.net/test?retryWrites=true&w=majority";
+const connection = "mongodb+srv://jasper:9e05bdaZimiYBCZk@cluster0-csq5x.gcp.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
